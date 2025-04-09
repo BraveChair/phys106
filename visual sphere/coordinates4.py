6f276db773509004be8f099118f387b2d7eb82da
BraveChair <courtlandgrace@gmail.com>
2025-04-09 01:15:24 +0000

    Cleaned up

--- visual sphere/coordinates4.py
+++ visual sphere/coordinates4.py
@@ -2,6 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+# Parameters
+R = 1  # Radius of the sphere
+n = 10000  # Number of random points
+epsilon = np.finfo(float).eps
+
 # Function to convert spherical to Cartesian coordinates
 def spheric2cartesian(r, theta, phi):
     x = r * np.sin(theta) * np.cos(phi)
@@ -9,41 +14,45 @@ def spheric2cartesian(r, theta, phi):
     z = r * np.cos(theta)
     return x, y, z
 
-# Parameters
-R = 1  # Radius of the sphere
-n = 1000  # Number of random points
+def generate_sphere(r):
+    # Generate random angles (Full sphere)
+    thetap_full = np.arccos(1 - 2 * np.random.rand(n))  # Theta: 0 to π
+    phip_full =  2* np.pi * np.random.rand(n) #n  # Phi: 0 to 2π  
 
-# Generate random angles (Full sphere)
-thetap_full = np.arccos(1 - 2 * np.random.rand(n))  # Theta: 0 to π
-phip_full =  2* np.pi * np.random.rand(n) #n  # Phi: 0 to 2π  
+    # Convert spherical coordinates to Cartesian
+    X_full, Y_full, Z_full = spheric2cartesian(R, thetap_full, phip_full)
 
+    # Set up the figure and 3D axis
+    return X_full,Y_full,Z_full
 
-# Convert spherical coordinates to Cartesian
-X_full, Y_full, Z_full = spheric2cartesian(R, thetap_full, phip_full)
+def show_sphere( X_full, Y_full, Z_full,): # Plot the random points on the sphere
+    
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(X_full, Y_full, Z_full, color="royalblue", s=3, alpha=0.8)
+    
+    # Set labels and aspect ratio
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
 
-# Set up the figure and 3D axis
-fig = plt.figure(figsize=(10, 10))
-ax = fig.add_subplot(111, projection='3d')
+    # Set equal aspect ratio for all axes
+    # ax.set_box_aspect([1, 1, 1])
+    ax.set_title('Monte Carlo Sphere')
+    plt.show()
 
-# Plot the random points on the sphere
-ax.scatter(X_full, Y_full, Z_full, color="royalblue", s=3, alpha=0.8)
+def sphere_bound_testing(R, X_full, Y_full, Z_full):
+    coords = np.vstack([X_full, Y_full, Z_full])
+    bad_points = np.any((coords < -R - epsilon) | (coords > R + epsilon), axis = 0)
+    if np.any(bad_points):
+        print("Bad Coordinates: ", coords[:,bad_points[1]])
+        raise ValueError("DATA ERROR: Some points are outside the sphere bounds.")
+    else: print("All points within bounds!")
 
-# Wireframe: Equator (phi = π/2)
-theta_eq = np.pi / 2
-phi_eq = np.linspace(0, 2 * np.pi, 100)  # Equator circle
-X_eq, Y_eq, Z_eq = spheric2cartesian(R, theta_eq, phi_eq)
-ax.plot(X_eq, Y_eq, Z_eq, color="red", lw=2)
+def main():
+    X_full,Y_full,Z_full = generate_sphere(R)
+    sphere_bound_testing(R,X_full,Y_full,Z_full)
+    # show_sphere(fig1,X_full,Y_full,Z_full)
 
-# Set labels and aspect ratio
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-
-# Set equal aspect ratio for all axes
-ax.set_box_aspect([1, 1, 1])
-
-# Set title
-ax.set_title('Sphere with Equator Border')
-
-# Show the plot
-plt.show()
+if __name__ == "__main__":
+    main()
